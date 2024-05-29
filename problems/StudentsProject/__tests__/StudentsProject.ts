4cf8e8cec30823a68323d1376d291f911ae9f18c
Cat4848 <catalinrol@gmail.com>
2024-05-29 10:05:08 +0100

    refactor: unpaired student test name

--- problems/StudentsProject/__tests__/StudentsProject.ts
+++ problems/StudentsProject/__tests__/StudentsProject.ts
@@ -1,6 +1,6 @@
 import { StudentsProject } from "../StudentsProject";
 
-test("if the correct odd student is returned", () => {
+test("if the correct unpaired student is returned", () => {
   const studentsID = [2, 4, 5, 6, 4, 2];
   const studentsProject = new StudentsProject(studentsID);
   expect(studentsProject.getUnpairedStudents()).toStrictEqual(new Set([5, 6]));
