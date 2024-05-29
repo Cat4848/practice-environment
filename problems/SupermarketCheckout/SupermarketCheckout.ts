c8055f90e11776f270b438e7a982f03990955b21
Cat4848 <catalinrol@gmail.com>
2024-05-29 12:49:06 +0100

    feat: product database class

--- problems/SupermarketCheckout/SupermarketCheckout.ts
+++ problems/SupermarketCheckout/SupermarketCheckout.ts
@@ -1,3 +1,18 @@
-class ShoppingBasket{
-  
+class ShoppingBasket {}
+
+export class ProductsDatabase {
+  async getProducts(url: string) {
+    try {
+      const products = await this.#getProductsFromUrl(url);
+      return products;
+    } catch (error) {
+      throw new Error("Issue loading products from web link");
+    }
+  }
+
+  async #getProductsFromUrl(url: string) {
+    const request = await fetch(url);
+    const products = await request.json();
+    return products;
+  }
 }
--- problems/SupermarketCheckout/__tests__/SupermarketCheckout.test.ts
+++ problems/SupermarketCheckout/__tests__/SupermarketCheckout.test.ts
@@ -1,3 +1,8 @@
-test("if the shopping basket contents are correct", () => {
+import { ProductsDatabase } from "../SupermarketCheckout";
 
+test("if the shopping basket contents are correct", async () => {
+  const db = new ProductsDatabase();
+  const productsUrl =
+    "https://s3.eu-west-1.amazonaws.com/hackajob-assets1.p.hackajob/challenges/sainsbury_products/products.json";
+  const products = await db.getProducts(productsUrl);
 });
