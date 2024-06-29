9dc81822978f406fe6db2987ac5c0e962d1b54f5
Cat4848 <catalinrol@gmail.com>
2024-06-29 21:00:29 +0100

    refactor

--- src/nodePlayground/streams/RandomStream/RandomStream.ts
+++ src/nodePlayground/streams/RandomStream/RandomStream.ts
@@ -3,19 +3,19 @@ import { Chance } from "chance";
 
 const chance = new Chance();
 
-// export default class RandomStream extends Readable {
-//   // emittedBytes = 0;
-//   // constructor(options){
-//   //   super(options);
-//   //   this.emittedBytes = 0;
-//   // }
-//   details() {
-//     console.log("print details");
-//   }
+export default class RandomStream extends Readable {
+  // emittedBytes = 0;
+  // constructor(options){
+  //   super(options);
+  //   this.emittedBytes = 0;
+  // }
+  details() {
+    console.log("print details");
+  }
 
-//   hello() {
-//     console.log("print hello");
-//   }
-// }
+  hello() {
+    console.log("print hello");
+  }
+}
 
 
