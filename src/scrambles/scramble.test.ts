c40414b5333146b3aa3901aeb0a9caad89813594
Cat4848 <catalinrol@gmail.com>
2024-10-18 05:57:38 +0100

    refactor

--- src/scrambles/scramble.test.ts
+++ src/scrambles/scramble.test.ts
@@ -1,6 +1,20 @@
-test('if arrays are equal' , () =>{
-  const s = 'flight';
+test("if arrays are equal", () => {
+  function* fibonacciSequence() {
+    let x = 0,
+      y = 1;
+    for (;;) {
+      yield y;
+      [x, y] = [y, x + y];
+    }
+  }
 
+  // const fib = fibonacciSequence();
 
-  console.log('indexOf', s.indexOf('flo'));
-})
+  function fibonacci(n: number) {
+    for (const f of fibonacciSequence()) {
+      console.log("fib number", f, n);
+      if (n-- <= 0) return f;
+    }
+  }
+  console.log("20th number", fibonacci(20));
+});
