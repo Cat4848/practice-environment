4c60ab32f928c659b6b31d423347eb68b6cb5d7c
Cat4848 <catalinrol@gmail.com>
2024-10-29 05:39:42 +0000

    matchAll method

--- src/scrambles/scramble.test.ts
+++ src/scrambles/scramble.test.ts
@@ -1,18 +1,6 @@
 test("if arrays are equal", () => {
-  function* fibonacciSequence() {
-    let x = 0,
-      y = 1;
-    for (;;) {
-      yield y;
-      [x, y] = [y, x + y];
-    }
-  }
+  const a = '1001';
+  const res = a.matchAll(/1/g);
+  const spred = [...res];
 
-  const fib = fibonacciSequence();
-
-  function fibonacci(n: number) {
-    for (const f of fibonacciSequence()) {
-      if (n-- <= 0) return f;
-    }
-  }
 });
