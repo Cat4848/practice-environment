b376904ae2d244ba54ee6e95c7105bfdcaf04d5b
Cat4848 <catalinrol@gmail.com>
2024-11-04 03:32:20 +0000

    refactor

--- src/nodePlayground/TaskQueue/__test__/TaskQueue.test.ts
+++ src/nodePlayground/TaskQueue/__test__/TaskQueue.test.ts
@@ -7,5 +7,5 @@ test("push resolve into the consumerQueue array", async () => {
   const resolveCallResult = await taskQueue.consumerQueue[0]();
   console.log("resolveCallResult", resolveCallResult);
   const a = ["a", "b", "c"];
-  console.log("concat", a.concat("d"));
+  // console.log("concat", a.concat("d"));
 });
