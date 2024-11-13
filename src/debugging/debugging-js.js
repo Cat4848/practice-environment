d5615f045251bcac4f2778e776a5efff603f19c1
Cat4848 <catalinrol@gmail.com>
2024-11-13 05:32:18 +0000

    feat: added debugging

---
+++ src/debugging/debugging-js.js
@@ -0,0 +1,14 @@
+const a = 4;
+const b = 5;
+const add = (num1, num2) => num1 + num2;
+
+console.log("here 1");
+console.info("here 2");
+
+function addition(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+
+add(a, b);
+addition(a, b);
---
+++ src/debugging/debugging-ts.ts
@@ -0,0 +1,13 @@
+const a = 4;
+const b = 5;
+const add = (num1: number, num2: number) => num1 + num2;
+
+console.log("here 1");
+console.info("here 2");
+function addition(num1: number, num2: number) {
+  const result = num1 + num2;
+  return result;
+}
+
+add(a, b);
+addition(a, b);
