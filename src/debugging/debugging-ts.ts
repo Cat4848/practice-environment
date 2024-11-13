f680ea8bc12d4ed204e4f04af64f7b809c4d2794
Cat4848 <catalinrol@gmail.com>
2024-11-13 06:11:38 +0000

    chore: removed the dist dir from git tracking

--- src/debugging/debugging-ts.ts
+++ src/debugging/debugging-ts.ts
@@ -1,13 +1,13 @@
-const a = 4;
-const b = 5;
-const add = (num1: number, num2: number) => num1 + num2;
+const c = 4;
+const d = 5;
+const add1 = (num1: number, num2: number) => num1 + num2;
 
 console.log("here 1");
 console.info("here 2");
-function addition(num1: number, num2: number) {
+function addition1(num1: number, num2: number) {
   const result = num1 + num2;
   return result;
 }
 
-add(a, b);
-addition(a, b);
+add1(c, d);
+addition1(c, d);
