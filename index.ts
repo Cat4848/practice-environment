b722054a4c190cdb10c8f65e6b1e8765a52a35c8
Cat4848 <catalinrol@gmail.com>
2024-11-14 04:40:41 +0000

    refactor

--- index.ts
+++ index.ts
@@ -4,10 +4,11 @@ const add = (num1: number, num2: number) => num1 + num2;
 
 console.log("here 1");
 console.info("here 2");
+console.warn("here 3");
 function addition(num1: number, num2: number) {
   const result = num1 + num2;
   return result;
 }
 
 add(a, b);
-addition(a, b);
+// addition(a, b);
